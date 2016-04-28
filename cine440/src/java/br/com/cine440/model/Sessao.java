5271de848c3fadde4ca3dfa3da2c3b53bc16ef67
aliciarodrigues <aliciarodrigues@senac.com>
2016-04-28 14:58:29 -0300

    Terceiro Commit

--- cine440/src/java/br/com/cine440/model/Sessao.java
+++ cine440/src/java/br/com/cine440/model/Sessao.java
@@ -1,11 +1,9 @@
-package br.com.cine440.model;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package br.com.cine440.model;
 
 /**
  *
