d9c2f3a5dcd39409703263f6d9914c205c4f219b
aliciarodrigues <aliciarodrigues@senac.com>
2016-04-29 15:57:14 -0300

    Decimo quinto Commit

--- projeto_cine440/src/java/br/com/projeto_cine440/bean/CadastroBean.java
+++ projeto_cine440/src/java/br/com/projeto_cine440/bean/CadastroBean.java
@@ -1,4 +1,94 @@
 package br.com.projeto_cine440.bean;
+
+import br.com.projeto_cine440.dao.CadastroDAO;
+import br.com.projeto_cine440.model.Cadastro;
+import br.com.projeto_cine440.util.JSFUtil;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+
 public class CadastroBean {
     
+    private Cadastro cadastro;
+    private ArrayList<Cadastro> itens;
+
+    public Cadastro getCadastro() {
+        return cadastro;
+    }
+
+    public void setCadastro(Cadastro cadastro) {
+        this.cadastro = cadastro;
+    }
+
+    
+
+    public ArrayList<Cadastro> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<Cadastro> itens) {
+        this.itens = itens;
+    }
+
+    @PostConstruct
+    public void prepararPesquisar() {
+        try {
+            CadastroDAO dao = new CadastroDAO();
+            itens = dao.listar();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void prepararCadastro() {
+        cadastro = new Cadastro();
+    }
+
+    public void novoCadastro() {
+        try {
+            CadastroDAO dao = new CadastroDAO();
+            dao.salvar(cadastro);
+            itens = dao.listar();
+            JSFUtil.addMsgSucesso("Cadastro salvo com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JSFUtil.addMsgErro(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtil.addMsgErro(ex.getMessage());
+        }
+    }
+
+    public void excluirCadastro() {
+        try {
+            CadastroDAO dao = new CadastroDAO();
+            dao.excluir(cadastro);
+            itens = dao.listar();
+            JSFUtil.addMsgSucesso("Cadastro excluido com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JSFUtil.addMsgErro(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtil.addMsgErro(ex.getMessage());
+        }
+    }
+
+    public void editarCadastro() {
+        try {
+            CadastroDAO dao = new CadastroDAO();
+            dao.editar(cadastro);
+            itens = dao.listar();
+            JSFUtil.addMsgSucesso("Cadastro alterado com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JSFUtil.addMsgErro(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtil.addMsgErro(ex.getMessage());
+        }
+    }
 }
+
--- projeto_cine440/src/java/br/com/projeto_cine440/bean/SessaoBean.java
+++ projeto_cine440/src/java/br/com/projeto_cine440/bean/SessaoBean.java
@@ -1,5 +1,93 @@
-
 package br.com.projeto_cine440.bean;
+
+import br.com.projeto_cine440.dao.SessaoDAO;
+import br.com.projeto_cine440.model.Sessoes;
+import br.com.projeto_cine440.util.JSFUtil;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+
 public class SessaoBean {
     
+    private Sessoes sessao;
+    private ArrayList<Sessoes> itens;
+
+    public Sessoes getSessao() {
+        return sessao;
+    }
+
+    public void setSessao(Sessoes sessao) {
+        this.sessao = sessao;
+    }
+
+    
+
+    public ArrayList<Sessoes> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<Sessoes> itens) {
+        this.itens = itens;
+    }
+
+    @PostConstruct
+    public void prepararPesquisar() {
+        try {
+            SessaoDAO dao = new SessaoDAO();
+            itens = dao.listar();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void prepararSessao() {
+        sessao = new Sessoes();
+    }
+
+    public void novaSessao() {
+        try {
+            SessaoDAO dao = new SessaoDAO();
+            dao.salvar(sessao);
+            itens = dao.listar();
+            JSFUtil.addMsgSucesso("Sessão salva com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JSFUtil.addMsgErro(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtil.addMsgErro(ex.getMessage());
+        }
+    }
+
+    public void excluirSessao() {
+        try {
+            SessaoDAO dao = new SessaoDAO();
+            dao.excluir(sessao);
+            itens = dao.listar();
+            JSFUtil.addMsgSucesso("Produto excluido com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JSFUtil.addMsgErro(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtil.addMsgErro(ex.getMessage());
+        }
+    }
+
+    public void editarSessao() {
+        try {
+            SessaoDAO dao = new SessaoDAO();
+            dao.editar(sessao);
+            itens = dao.listar();
+            JSFUtil.addMsgSucesso("Sessão alterada com sucesso.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JSFUtil.addMsgErro(e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtil.addMsgErro(ex.getMessage());
+        }
+    }
 }
