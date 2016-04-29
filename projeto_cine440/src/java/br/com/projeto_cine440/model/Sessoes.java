78481d9ad79b1aa0957570951c2673d72f7e37b2
aliciarodrigues <aliciarodrigues@senac.com>
2016-04-29 14:31:38 -0300

    Decimo primeiro Commit

--- projeto_cine440/src/java/br/com/projeto_cine440/model/Sessoes.java
+++ projeto_cine440/src/java/br/com/projeto_cine440/model/Sessoes.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.projeto_cine440.model;
 
-/**
- *
- * @author Leonan
- */
+import java.sql.Time;
+
 public class Sessoes {
+    private int id_sessao;
+    private String filmes;
+    private double preco;
+    private Time horario;
+
+    public int getId_sessao() {
+        return id_sessao;
+    }
+
+    public void setId_sessao(int id_sessao) {
+        this.id_sessao = id_sessao;
+    }
+
+    public String getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(String filmes) {
+        this.filmes = filmes;
+    }
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Time getHorario() {
+        return horario;
+    }
+
+  
+    public void setHorario(Time horario) {
+        this.horario = horario;
+    }
+    
     
 }
